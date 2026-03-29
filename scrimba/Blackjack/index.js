8791d5e300f6b2c84ce7728ce46cfb909588660f
Aaron chifamba <143525080+aj-kc@users.noreply.github.com>
2026-03-29 21:57:18 +0100

    started  blackjack

---
+++ scrimba/Blackjack/index.js
@@ -0,0 +1,30 @@
+let firstCard = 20
+let secondCard = 11
+let age = 100
+
+let sum = firstCard + secondCard
+
+console.log(sum)
+console.log(age)
+
+if (sum < 21){
+   console.log("Do you want to draw a new card?")   
+}
+
+    else if(sum === 21){
+        console.log("You have won!")
+    }
+
+        else{
+            console.log("You have gone Bust!")
+        }
+
+
+
+if (age > 20){
+    console.log("Welcome to the club!")
+}
+
+    else{
+        console.log("You cannnot come in.")
+    }
