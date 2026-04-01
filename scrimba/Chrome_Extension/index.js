6040feec7e002f8d2e3aed479539c3c33c4d7375
Chifamba Aaron (2025) ZPAC415 <aaron.chifamba.2025@live.rhul.ac.uk>
2026-04-01 12:07:24 +0100

    deployed extension

--- scrimba/Chrome_Extension/index.js
+++ scrimba/Chrome_Extension/index.js
@@ -3,18 +3,31 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-function renderLeads() {
-  ulEl.innerHTML = ""
-  for (let i = 0; i < myLeads.length; i++) {
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.appendChild(li)
-    console.log(myLeads[i])
-  }
-}
-
 inputBtn.addEventListener("click", function() {
   myLeads.push(inputEl.value)
+  inputEl.value = ""
   console.log(myLeads)
   renderLeads()
 })
+
+function renderLeads()
+{
+    let listItems = ""
+
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems +=
+         `<li>
+            <a target = '_blank' href = '${myLeads[i]}'>
+                 ${myLeads[i]}
+            </a>
+          </li>`
+    }
+
+    ulEl.innerHTML = listItems
+}
+
+const recipient = "James"
+const sender = "Aaron"
+
+const email = `Hey ${recipient} How is it going? Cheers ${sender}.`
+console.log(email)
